fefb0f5f6110cf68b2a74b29e33c7be5e93feb13
JoshuaYeoh1 <joshuayeohkeatkeong@gmail.com>
2024-11-27 23:55:47 +0800

    Sprite Echo fix

---
+++ FYP_One Last Time/Assets/Scripts/Yeoh/NiceToHaves/VelocityMeter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityMeter : MonoBehaviour
+{
+    public GameObject owner;
+
+    [Header("Debug")]
+    public Vector3 velocity;
+    public float velocityMagnitude;
+    public Vector3 velocityDirection;
+
+    Vector3 prevPos;
+
+    void FixedUpdate()
+    {
+        velocity = (owner.transform.position - prevPos) / Time.deltaTime;
+        velocityMagnitude = velocity.magnitude;
+        velocityDirection = velocity.normalized;
+
+        prevPos = owner.transform.position;
+    }
+}
