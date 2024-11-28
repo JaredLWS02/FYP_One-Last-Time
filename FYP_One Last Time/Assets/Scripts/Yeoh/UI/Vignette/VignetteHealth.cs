b24d9446f095d841bd5ec00fc4c215c683a956e3
JoshuaYeoh1 <joshuayeohkeatkeong@gmail.com>
2024-11-29 01:52:51 +0800

    Update VignetteHealth.cs

--- FYP_One Last Time/Assets/Scripts/Yeoh/UI/Vignette/VignetteHealth.cs
+++ FYP_One Last Time/Assets/Scripts/Yeoh/UI/Vignette/VignetteHealth.cs
@@ -16,12 +16,32 @@ public class VignetteHealth : MonoBehaviour
 
     public HPManager hpM;
     public Color vignetteColor = Color.red;
+    public float minAlpha=0;
+    public float maxAlpha_=.75f;
     
     void Update()
     {
         if(!hpM) return;
-        float inverted_percent = 100 - hpM.GetHPPercent();
-        vignetteColor.a = inverted_percent / 100;
+
+        float hp01 = GetValue01(hpM.hp, 0, hpM.hpMax);
+        vignetteColor.a = Mathf.Lerp(minAlpha, maxAlpha_, 1-hp01);
+
         vignette.color = vignetteColor;
+    }
+
+    // ============================================================================
+
+    float GetValue01(float current, float min, float max)
+    {
+        if(current <= min) return 0;
+        if(current >= max) return 1;
+        
+        float range = max - min;
+        if(range<=0) return 0;
+
+        float offset = current - min;
+        float value01 = offset / range;
+
+        return Mathf.Clamp01(value01);
     }
 }
