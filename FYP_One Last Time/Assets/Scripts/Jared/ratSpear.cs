8800bb3c477f7c06f2d54206d35e1f090c42bfa4
JaredLWS <0135860@kdu-online.com>
2025-03-17 03:36:26 +0800

    finished projectile

--- FYP_One Last Time/Assets/Scripts/Jared/ratSpear.cs
+++ FYP_One Last Time/Assets/Scripts/Jared/ratSpear.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ratSpear : MonoBehaviour
 {
+    private GameObject target;
+    private Rigidbody rb;
+    private float force;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody>();
+        force = 10.0f;
+
+        if (target != null)
+        {
+            Vector3 direction = target.transform.position - transform.position;
+            rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+            float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, rot);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision col)
     {
-        
+        if (col.gameObject.tag == "Player")
+        {
+            //deal dmg
+            Destroy(gameObject);
+        }
     }
 }
