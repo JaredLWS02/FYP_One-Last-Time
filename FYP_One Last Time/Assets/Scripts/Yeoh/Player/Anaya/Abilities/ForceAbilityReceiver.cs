01e2c8701dff278ece25b87c2629d0b9c715b2c1
JoshuaYeoh1 <joshuayeohkeatkeong@gmail.com>
2025-04-12 20:33:50 +0800

    rename

--- FYP_One Last Time/Assets/Scripts/Yeoh/Player/Anaya/Abilities/ForceAbilityReceiver.cs
+++ FYP_One Last Time/Assets/Scripts/Yeoh/Player/Anaya/Abilities/ForceAbilityReceiver.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class ForceReceiver : MonoBehaviour
+public class ForceAbilityReceiver : MonoBehaviour
 {
     public GameObject owner;
     public Rigidbody rb;
