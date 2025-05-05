2d952387281a1ab51cb34ea300c4d55e19324af3
JoshuaYeoh1 <joshuayeohkeatkeong@gmail.com>
2025-05-05 09:53:27 +0800

    Log success/fail on enter trigger

--- FYP_One Last Time/Assets/Scripts/Yeoh/NiceToHaves/MyDebug.cs
+++ FYP_One Last Time/Assets/Scripts/Yeoh/NiceToHaves/MyDebug.cs
@@ -5,5 +5,7 @@ using UnityEngine;
 public class MyDebug : MonoBehaviour
 {
     public void Log(string msg) => Debug.Log(msg);
+    public void LogWarning(string msg) => Debug.LogWarning(msg);
+    public void LogError(string msg) => Debug.LogError(msg);
     public void Print(string msg) => print(msg);
 }
